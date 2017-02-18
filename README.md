36f71bbffd36fd7a4c1c0512c4eae1c06a2fa931
CJ Walker <cjwalker.pdx@gmail.com>
2017-02-17 17:07:33 -0800

    Submit class_04 changes to guessing-game

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # guessing-game
+
+Short personal bio page practicing html and css. 3 question guessing game built with js prompts with answers printed to the DOM.
+
+## Motivation
+
+Homework
+
+## Contributors
+
+N/A
+
+## License
+
+N/A
--- script.js
+++ script.js
@@ -1,65 +1,150 @@
 var count=0
-var userName = prompt('What\'s your name?',"");
-alert("Welcome " + userName + "!" + "\n" + "I'm glad you're here.");
 
+var userName;
 
-function yesOrNo() {
-   var answer=prompt("Hey " + userName + ", are you ready to play a guessing game about CJ?","Yes/No");
-   if (answer== "Yes"){
-        alert("Great!");
-        count++;
-   } else {
-   alert("Too bad - it\'s time for some more pop-up windows alerts, buster.");
-   };
- }
-  console.log("if/else for yes no question");
-yesOrNo();
+function foodGreeting() {
+  var time=new Date();
+  var timeToday = time.getHours();
+  switch (true) {
+    case (timeToday < 10): {
+      alert("Welcome " + userName + " - " + "I hope you had a good breakfast today");
+      break;
+    }
+    case (timeToday < 12): {
+      alert("Welcome " + userName + " - " + "I hope you ate brunch today");
+      break;
+    }
+    case (timeToday < 17): {
+      alert("Welcome " + userName + " - " + "I hope you had a yummy lunch");
+      break;
+    }
+    case (timeToday >= 17): {
+      alert("Welcome " + userName + " - " + "I hope your dinner tonight was delicious");
+      break;
+    }
+    default: {
+      alert("Welcome " + userName + " - " + "have a cup of tea!");
+      break
+    }
+  }
+};
 
+console.log("Switch statement for food greeting");
 
 function whiskeyOrBeer() {
-  var favoriteDrink=prompt("Next:" + "\n" + "Is whiskey or beer their drink of choice?","");
-  if (favoriteDrink=="Whiskey") {
-    alert("It sure is.");
+  var favoriteDrink=prompt("Is whiskey or beer CJ\'s drink of choice?","");
+  if (favoriteDrink.toLowerCase()==="whiskey") {
+
+    var userSays = document.getElementById("userResponse1")
+    userSays.innerHTML="Your response: " + favoriteDrink
+    var gameSays = document.getElementById("answer1")
+    gameSays.innerHTML="Answer: It sure is whiskey!"
+    console.log("case matching");
     count++;
-  } else if (favoriteDrink=="Beer") {
-    alert("Nope.");
-  };
+
+  } else if(favoriteDrink.toLowerCase()==="beer") {
+
+    var userSays = document.getElementById("userResponse1")
+    userSays.innerHTML="Your response: " + favoriteDrink
+    var gameSays = document.getElementById("answer1")
+    gameSays.innerHTML="Answer: Nope, it's whiskey."
+
+  } else {
+
+    var userSays = document.getElementById("userResponse1")
+    userSays.innerHTML="Your response: " + favoriteDrink
+    var gameSays = document.getElementById("answer1")
+    gameSays.innerHTML="Answer: Hey, the options were whiskey or beer..."
+  }
   console.log("else if for favoriteDrink");
-}
-whiskeyOrBeer();
+};
 
 function westCoastBestCoast() {
-  var whichCoast=prompt("Okay! Next:" + "\n" + "Does CJ prefer the East Coast or the West Coast?","");
-  if (whichCoast=="West coast") {
-    alert("Yes!");
+  var whichCoast=prompt("Okay! Next:" + "\n" + "Do they prefer the East Coast or the West Coast?","");
+  if (whichCoast.toLowerCase()==="west coast") {
+
+    var userSays = document.getElementById("userResponse2")
+    userSays.innerHTML="Your response: " + whichCoast
+    var gameSays = document.getElementById("answer2")
+    gameSays.innerHTML="Answer: Yes! West coast is the best coast."
+    console.log("case matching");
     count++;
-    }
-  if (whichCoast=="East coast") {
-    alert("Nope . . . the West Coast is home.");
-  };
-  console.log("if else for west coast");
-}
-westCoastBestCoast();
+
+  } else if (whichCoast.toLowerCase()==="east coast") {
+    var userSays = document.getElementById("userResponse2")
+    userSays.innerHTML="Your response: " + whichCoast
+    var gameSays = document.getElementById("answer2")
+    gameSays.innerHTML="Answer: Nope, the West Coast is home."
+
+  } else {
+    var userSays = document.getElementById("userResponse2")
+    userSays.innerHTML="Your response: " + whichCoast
+    var gameSays = document.getElementById("answer2")
+    gameSays.innerHTML="Answer: Hey, the options were West Coast or East Coast . . ."
+  }
+  console.log("if else if for west coast");
+};
 
 function howManyCats() {
   var catCount=prompt("How many cats does CJ have?","");
-  if (catCount==2 || "two") {
-    alert("Exactly!");
-    count++;
-  } else if (catCount<2 || "one"){
-    alert("More then that . . .")
-  } else {("Too much fluff.")}
-  console.log("if else with numbers for how many cats")
-  };
-howManyCats();
 
-alert("You got " + count + " out of 4 right.");
+  if (parseInt(catCount)===2 || catCount.toLowerCase()==="two") {
+
+    var userSays = document.getElementById("userResponse3")
+    userSays.innerHTML="Your response: " + catCount
+    var gameSays = document.getElementById("answer3")
+    gameSays.innerHTML="Answer: Yes - exactly two cats!"
+    count++;
+
+  } else if (parseInt(catCount)<2 || catCount.toLowerCase()==="one"){
+
+    var userSays = document.getElementById("userResponse3")
+    userSays.innerHTML="Your response: " + catCount
+    var gameSays = document.getElementById("answer3")
+    gameSays.innerHTML="Answer: Nope, more cats than that."
+
+  } else {
+    var userSays = document.getElementById("userResponse3")
+    userSays.innerHTML="Your response: " + catCount
+    var gameSays = document.getElementById("answer3")
+    gameSays.innerHTML="Answer: Nonono that's way too much fluff!"
+  };
+  console.log("if else with numbers for how many cats");
+};
+/*
+function favoriteColor() {
+var answer=prompt("Is CJ\'s favorite color green, blue, or purple? ,"");
+if (favoriteColor.toLowerCase()==="blue") {
+
+var colorOptions = ()"blue", "green", "purple")
+for (var i = 0; i < colorOptions.length; i++) {
+if(colorOptions[i] === "blue" {
+document.getElementById("answer-link").innerHTML+=("Yes, blue is their favorite.");
+count++;
+
+})
+}
+
+*/
+function playGame() {
+  userName=prompt("Hi there! What\'s your name?","");
+  foodGreeting();
+  whiskeyOrBeer();
+  westCoastBestCoast();
+  howManyCats();
+  /*
+  favoriteColor();
+  */
+  document.getElementById("questionCount").innerHTML+=("Well " + userName + ", you got " + count + " out of 3 right.");
+};
 
 
 
 
 /*
-A couple things are still misfiring here. whiskeyOrBeer answer "beer" doesn't trigger alert - just goes to next question. And in howMany Cats I thought that by using a general "else" for numbers >2 that would fire regardless of strong or integer input, but it's that only sometimes seems to be happening.
-I'm going to check this in and move ahead.
-Also, some css color showing in the browser - that's jumping ahead a bit to Tues, even though this submission is catching up on Saturday's homework,
+Notes:
+I had a question "would you like to play a game?" which got replaced in the flow by the addition of the "play game" button. So I'm currently down a question from the number I should have. That question, whiskeyOrBeer, and westCoastBestCoast all had the same programming so I didn't build a replacement question since that format is already demo-ed within the current questio set.
+Based on the homework I'm currently missing 3 questions from this homework - the for loop, one of the original 3 (dropped per above), and one more that I can't seem to chase down the provinance of in the assignments. I'm going to submit as is with the knowledge this might be partial credit and a re-submit later.
+For loop is in progress but currently commented out.
+Table is outside of assignment scope, but I was having fun!
 */
